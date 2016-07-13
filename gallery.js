cb31ef5f8e38646711254b593c3fbd8515cb763a
Jannis R <mail@jannisr.de>
2016-07-13 16:37:53 +0200

    gallery :sparkles:

---
+++ gallery.js
@@ -0,0 +1,79 @@
+'use strict'
+
+const Masonry = require('masonry-layout')
+
+const qs = require('./querystring')
+
+
+
+const gallery = document.getElementById('gallery')
+
+const dom = (tag, props, content) => {
+	const el = document.createElement(tag)
+	for (let key in props) el.setAttribute(key, props[key])
+	if (Array.isArray(content)) {
+		for (let child of content) el.appendChild(child)
+	} else if ('string' === typeof content) el.innerHTML = content
+	return el
+}
+
+
+
+const matcher = (query) => (photo) => {
+	for (let filter in query) {
+		if (query[filter] !== photo[filter]) return false
+	}
+	return true
+}
+
+
+
+let photos = []
+
+const setData = (newPhotos) => {
+	photos = []
+	for (let photo of newPhotos) {
+		photo = Object.create(photo)
+		photo.el = dom('a', {
+			  class: 'picture'
+			, href: '#'
+			, role: 'button'
+			, 'aria-haspopup': 'true'
+		}, [
+			dom('img', {src: photo.url})
+		])
+		photos.push(photo)
+	}
+	applyQuery(qs.current)
+}
+
+
+
+const masonry = new Masonry(gallery, {
+	itemSelector:       '#gallery .picture',
+	fitWidth:           true,
+	transitionDuration: 0
+})
+
+const applyQuery = (query) => {
+	console.log('applyQuery', query)
+	for (let shown of gallery.childNodes) gallery.removeChild(shown)
+
+	const matches = matcher(query)
+	const toBeShown = []
+	for (let photo of photos) {
+		console.log(query, photo)
+		if (matches(photo)) toBeShown.push(photo.el)
+	}
+	console.log('toBeShown', toBeShown)
+
+	for (let el of toBeShown) gallery.appendChild(el)
+	masonry.appended(toBeShown)
+}
+
+module.exports = {setData, applyQuery}
+
+
+
+// URL -> Gallery
+qs.on('change', applyQuery)
--- index.js
+++ index.js
@@ -1,29 +1,20 @@
 'use strict'
 
-const Masonry = require('masonry-layout')
-
 const notify = require('./notify')
 const filters = require('./filters')
+const gallery = require('./gallery')
 const data = require('./data')
 
 
 
-const gallery = document.getElementById('gallery')
-const masonry = new Masonry(gallery, {
-	itemSelector:       '#gallery .picture',
-	fitWidth:           true,
-	transitionDuration: 0
-})
-
-
-
 const main = (data) => {
-	console.log('main')
 	for (let name of data.filters) {
 		const filter = data[name]
 		const values = Object.keys(filter)
 		filters.addFilter(name, values)
 	}
+
+	gallery.setData(data.photos)
 }
 
 data.then(main, (e) => notify(e.message))
