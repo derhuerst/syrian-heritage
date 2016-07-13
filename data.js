adfb79d4082eb56fdbefa29234a9e2764f4214b7
Jannis R <mail@jannisr.de>
2016-07-13 15:29:08 +0200

    fetch data, show filters

---
+++ data.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const fetch = require('isomorphic-fetch')
+const groupBy = require('lodash.groupBy')
+
+
+
+const endpoint = 'http://199.217.112.113:8080/picture/picture'
+const filters = ['collection', 'color', 'location', 'motif', 'view']
+
+
+
+const err = (res) => {
+	const e = new Error(res.statusText)
+	e.statusCode = res.status
+	throw e
+}
+
+const data = fetch(endpoint)
+.then((res) => res.json(), err)
+.then((photos) => {
+	const data = {}
+
+	data.photos = photos.map((p) => ({
+		  id:         p.id
+		, url:        p.Bildname
+		, collection: p.Collection
+		, color:      p.Color
+		, location:   p.Location
+		, motif:      p.Subject
+		, view:       p.View
+	}))
+
+	for (let filter of filters)
+		data[filter] = groupBy(data.photos, (p) => p[filter])
+
+	data.filters = filters
+	return data
+}, err)
+
+module.exports = data
--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const Masonry = require('masonry-layout')
 
 const notify = require('./notify')
+const filters = require('./filters')
+const data = require('./data')
 
 
 
@@ -12,3 +14,16 @@ const masonry = new Masonry(gallery, {
 	fitWidth:           true,
 	transitionDuration: 0
 })
+
+
+
+const main = (data) => {
+	console.log('main')
+	for (let name of data.filters) {
+		const filter = data[name]
+		const values = Object.keys(filter)
+		filters.addFilter(name, values)
+	}
+}
+
+data.then(main, (e) => notify(e.message))
