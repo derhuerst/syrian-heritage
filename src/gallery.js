8439c865b6816f89544b52cb50e62bbd14d8f158
Jannis R <mail@jannisr.de>
2016-07-22 21:31:49 +0200

    use yoyo & redux: gallery

---
+++ src/gallery.js
@@ -0,0 +1,49 @@
+'use strict'
+
+const yo = require('yo-yo')
+const Masonry = require('masonry-layout')
+
+
+
+const values = (obj) => {
+	const r = []
+	for (let key in obj) r.push(obj[key])
+	return r
+}
+
+
+
+const photo = (photo) => yo `
+	<a class="picture" href="${photo.url}" target="_blank"
+	   role="button" aria-haspopup="true">
+		<img src="${photo.thumb.url}
+		     width="${photo.thumb.width}" height="${photo.thumb.height}"/>
+	</a>`
+
+const cluster = (photos) => {
+	const dom = yo `
+		<section class="cluster">
+			${photos.map(photo)}
+		</section>`
+	const masonry = new Masonry(dom, {fitWidth: true, transitionDuration: 0})
+	masonry.appended(dom.childNodes)
+	masonry.layout()
+	return dom
+}
+
+const gallery = (photosByType) => yo `
+	<div>
+		${values(photosByType).map(cluster)}
+	<div>`
+
+
+
+let dom = gallery({})
+
+const render = (photosByType) => {
+	const newDom = render(photosByType)
+	yo.update(dom, newDom)
+	dom = newDom
+}
+
+module.exports = render
