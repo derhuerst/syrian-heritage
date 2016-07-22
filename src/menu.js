737d3e8143170e77746ca160edc3ffb3efa2a065
Jannis R <mail@jannisr.de>
2016-07-22 21:31:19 +0200

    use yoyo & redux: menu

---
+++ src/menu.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const yo = require('yo-yo')
+
+
+
+const menu = (currentCat, allCats, setCat, clearCat) => yo `
+	<ul>
+		${allCats.map((cat) => {
+			const onClick = (e) => {
+				e.preventDefault()
+				e.stopPropagation()
+				if (currentCat === cat) clearCat()
+				else setCat(cat)
+			}
+			return yo `
+			<li ${currentCat === cat ? 'class="active"' : ''}>
+				<a href="#" role="button" aria-haspopup="true"
+				   onclick="${onClick}">${cat}</a>
+			</li>
+			`
+		})}
+	<ul>`
+
+
+
+const noop = () => {}
+let dom = menu(null, [], noop, noop)
+
+const render = (currentCat, allCats, setCat, clearCat) => {
+	const newDom = menu(currentCat, allCats, setCat, clearCat)
+	yo.update(dom, newDom)
+	dom = newDom
+}
+
+module.exports = render
