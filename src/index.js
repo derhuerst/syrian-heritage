4fdf7db40404a11a2889bedb395ae0efbe244117
Jannis R <mail@jannisr.de>
2016-07-24 17:34:01 +0200

    use yoyo & redux: store, combine everything

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 'use strict'
 
-// todo
+const createStore = require('redux/lib/createStore').default
+
+const actions = require('./actions')
+const ui = require('./ui')
+const reducer = require('./reducer')
+const data = require('./data')
+
+
+
+const store = createStore(reducer)
+
+const setCategory = actions.setCategory(store)
+const clearCategory = actions.clearCategory(store)
+
+const render = ui(setCategory, clearCategory)
+store.subscribe(() => render(store.getState()))
+
+
+
+data.then(actions.setPhotos(store), (e) => {
+	// todo
+	// notify(e.message)
+	throw e
+})
+
+
+
+// todo: watch & update querystring
